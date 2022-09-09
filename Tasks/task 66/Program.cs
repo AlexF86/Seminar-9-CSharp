eb0d7df5a6a2aa9da577851c110b6782aaf847e1
Alexandr <don.alexande1986@yandex.ru>
2022-09-09 07:21:26 +0300

    задача 66

--- Tasks/task 66/Program.cs
+++ Tasks/task 66/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int Sum(int m, int n)
+{
+    if (n == m) return n;
+    return m + Sum(m + 1, n);
+}
+Console.WriteLine(Sum(1, 15));
+Console.WriteLine(Sum(4, 8));
