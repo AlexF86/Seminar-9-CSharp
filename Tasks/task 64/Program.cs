207367c679f11c5ddb4a0a008e2535a1fad1fa4d
Alexandr <don.alexande1986@yandex.ru>
2022-09-09 07:17:45 +0300

    задача 64

--- Tasks/task 64/Program.cs
+++ Tasks/task 64/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void PrintNumber(int m, int n)
+{
+    if (n < m) return;
+    PrintNumber(m, n - 1);
+    Console.Write(n + " ");
+}
+PrintNumber(1, 5);
+PrintNumber(4, 8);
